165df0248192f08bc941bf17d490e914a677ecb6
rektide <rektide@voodoowarez.com>
2013-01-28 02:01:50 -0500

    Slurp in sub.

--- modules/alsa.js
+++ modules/alsa.js
@@ -5,54 +5,74 @@ var Q= require("q"),
 
 var readDir= Q.nfbind(FS.readDir)
 
-var matches= []
+var AUTORUN= 3
 
-
-function playback(card,n){
-
+function matrixize(components){
+	var agg= [],
+	  k= []
+	for(var i= 0; i< arguments.legnth; ++i){
+		k= k.concat(arguments[i]
+		agg.push(k)
+		k= k.concat(null)
+		agg.push(k)
+	}
+	return agg
 }
 
-function capture(card,n){
-}
-
-function sub(c,p,s,isPlayback){
-        var base= ["/proc/asound/card",c,"/pcm"+p+isPlayback?"p/sub":"c/sub",s,null]
-	function _la(arr,v){
-		arr[arr.length-1]= v
-		return arr.join("")
+var addrMatrix= matrixize("/proc/asound/card","/pcm","","/sub")
+addrMatrix.phrase= _phrase
+function phrase(c,p,isPlaybackNotCapture,s){
+	var val
+	if(s){
+		val= this[5]
+		val[7]= s
+	}else if(p){
+		val= this[3]
 	}
 
-	var val= {}
-	var f= function(name){
-		var p= this[name]= readFile(_la(base,name)).then(function(){
-			if(--this.ref==0){
-				this.ready.resolve(this)
-			}
-			return this
-		}.bind(this))
-		l.push(p)
-		return p
-	}.bind(val)
-	var all= ["hw_params","info","prealloc","prealloc_max","status","sw_params"].map(function(v,i,arr){
-		f(v)
+	if(s||p){
+		val[5]= isPlaybackNotCapture? "p": "c"
+		val[3]= p
+	}else{
+		val= this[1]
+	}
+	val[1]= c
+	return val.join("")
+}
+
+var card= function(c){
+	
+}.bind(addrMatrix)
+
+var playback= function(c,p){
+	sub(c,p,0,true)
+	sub(c,p,1,true)
+	sub(c,p,2,true)
+}
+
+function capture(c,p){
+	sub(c,p,0,false)
+	sub(c,p,1,false)
+	sub(c,p,2,false)
+}
+
+function __ready(val){
+	if(--this.ref==0){
+		this.ready.resolve(this)
+	}
+	return val
+}
+
+function __readContext(name,addr){
+	return this[name]= readFile(addr).then(__ready.bind(this))
+}
+
+function sub(c,p,isPlayback,s){
+	var addr= this.phrase(c,p,isPlayback,s),
+	  val= {}
+	var all= ["hw_params","info","prealloc","prealloc_max","status","sw_params"].map(function(name,i,arr){
+		return __readyContext.call(val,name,addr)
 	})
 	return Q.all(all)
 }
-
-function slurpCard(n){
-	var id= readFile("/proc/asound/card"+n+"/id"),
-}
-
-function slurpCard(n,start){
-	n= n||0
-	start= start||[]
-	readDir("/proc/asound/card"+n).then(function(n,entries){
-		for(var e in entries){
-			var c= /pcm(\d+)c/.exec(data),
-			 p= /pcm(\d+)c/.exec(data),
-		}
-		slurpCard(this+1)
-	}.bind(start,n)).fail
-}
-
 
