4f442b214a5f1d4b76731c3f89e0020105b74b1e
rektide <rektide@voodoowarez.com>
2013-02-20 00:49:28 -0500

    Get trusted.gpg.d stuff running- needed entire keyfile. Add tasks/apt.srclist.install.tasks.

--- files/UTIL/get-apt-key.sh
+++ files/UTIL/get-apt-key.sh
@@ -12,12 +12,8 @@ NAME=$1
 KEY=$2
 KEYSERVER=$3
 [[ -z "$KEYSERVER" ]] && KEYSERVER=keys.gnupg.net
-TMP_KEYRING=`mktemp --suffix=-get-apt-key`
-echo Fetching $KEY from $KEYSERVER to $KEY.gpg
+echo Fetching key $KEY into $NAME.gpg
 
-gpg --keyserver $KEYSERVER --no-default-keyring --keyring $TMP_KEYRING --recv-keys $KEY
-# would that I understand why the retrieved key is not $KEY, or know what in general was desired to augment --export with desired target
-# gpg -a --export --keyring $TMP_KEYRING > $NAME.gpg
-gpg -a --export --keyring $TMP_KEYRING > $NAME.gpg
-rm $TMP_KEYRING
+gpg --no-default-keyring --keyring `pwd`/$NAME.gpg --keyserver $KEYSERVER --recv-keys $KEY
+rm $NAME.gpg~
 
--- x2go.pb
+++ x2go.pb
@@ -12,7 +12,7 @@
   - vars/common.vars
   tasks:
   - include: tasks/cfvar_includes.tasks
-  - include: tasks/apt.key.install.tasks name=$NAME
+  - include: tasks/apt.key.install.tasks name=$NAME.seed
   - include: tasks/apt.list.install.tasks name=$NAME.unstable
-  #- include: tasks/apt.list.install.tasks name=$NAME.unstable.src 
-  #- apt: state=${APT_INSTALL} pkg=
+  #- include: tasks/apt.srclist.install.tasks name=$NAME.unstable
+  - apt: state=${APT_INSTALL} pkg=x2go-keyring,x2goserver,x2godesktopsharing,x2goserver-extensions,x2go-client
