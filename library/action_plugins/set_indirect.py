1ec5fb68f2119a2935849bd9adc2e878b1ffaece
rektide <rektide@voodoowarez.com>
2013-11-14 00:41:23 -0500

    Give complex_arg reading capabilities to set_indirect action_plugin.

--- library/action_plugins/set_indirect.py
+++ library/action_plugins/set_indirect.py
@@ -11,11 +11,19 @@ class ActionModule(object):
     def run(self, conn, tmp, module_name, module_args, inject, complex_args=None, **kwargs):
         ''' handler for running operations on master '''
 
-        # load up options
-        options  = {}
-        if complex_args:
-            options.update(complex_args)
-        options.update(utils.parse_kv(module_args))
-        result=dict({'ansible_facts':{options['var']: options['val']}})
+        options= utils.parse_kv(module_args)
 
+        if not options['var']:
+            return ReturnData(conn=conn, result=dict(failed= True, msg="No var specified"))
+
+        k= options.get('var', None)
+        v= options.get("val", None)
+        if complex_args and v:
+            v= complex_args[v]
+        elif complex_args:
+            v= complex_args
+        elif not v:
+            return ReturnData(conn=conn, result=dict(failed= True, msg="No val for "+k))
+
+        result=dict({'ansible_facts':{k:v}})
         return ReturnData(conn=conn, result=result)
