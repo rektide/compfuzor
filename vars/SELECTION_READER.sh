69c49f22af6dca0cbceda3d95abba4db69ae17a1
rektide <rektide@voodoowarez.com>
2014-05-29 00:21:12 -0400

    Implement SELECTION_READER.sh fully.

--- vars/SELECTION_READER.sh
+++ vars/SELECTION_READER.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-debconf-get-selection | egrep '^\w*$1' | grep -v '^#' | 2json -s "program,key,type,value" -n -o "${2:-base_selections}" | json2yaml
+debconf-get-selections | egrep "^\w*$1" | grep -v '^#' | 2json -s "program,key,type,value" -n -w 1,1,1,300 | json2yaml | yamlnest "${2:-base_selections}"
