dfed345c6c02a08442b8a55891625ebaeafb2d7a
rektide <rektide@voodoowarez.com>
2014-06-02 19:02:32 -0400

    Begin a prepare-sd script.

---
+++ files/sama5d3-xplained/prepare-sd.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+[ -n "$1" ] || exit 1
+
+DISK="$1"
+
+sudo dd if=/dev/zero of=${DISK} bs=1M count=16
+
+sudo sfdisk --in-order --Linux --unit M ${DISK} <<-__EOF__
+1,48,0xE,*
+,,,-
+__EOF__
+
+PART1="${DISK}1"
+PART2="${DISK}2"
+if [ ! -b "${PART1}" ]
+then
+	PART1="${DISK}p1"
+	PART2="${DISK}p2"
+fi
+
+sudo mkfs.vfat -F 16 ${PART1} -n boot
+sudo mkfs.ext4 ${PART2} -L rootfs
