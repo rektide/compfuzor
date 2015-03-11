5766d56b7faba56c63b7d90b18ae13119256814e
rektide <rektide@voodoowarez.com>
2015-03-10 21:39:06 -0400

    Allow hw-sama5d3 part1 to run with an existing BOOT_MNT.

--- files/hw-sama5d3/part1-install-sd.sh
+++ files/hw-sama5d3/part1-install-sd.sh
@@ -2,19 +2,12 @@
 
 set -e
 
-if [ ! -b "$1" ]
-then
-	echo Not a block device to instal boot files into
-	exit 2
-fi
-
 [ -n "$2" ] && VAR="$2"
 [ -z "$VAR" ] && VAR="{{VAR}}"
+[ -z "$BOOT_MNT" ] && [ -b "$1 " ] && BOOT_MNT=`mktemp -d --suffix=boot-mnt --tmpdir=.` && sudo mount "$1" "${BOOT_MNT}"
 
 # copy boot files on to sd card
-BOOT_MNT=`mktemp -d --suffix=boot-mnt --tmpdir=.`
-sudo mount "${1}" "${BOOT_MNT}"
-sudo cp ${VAR}/u-boot-sd.spl "${BOOT_MNT}/BOOT.BIN"
+sudo cp ${VAR}/u-boot-spl-sd.bin "${BOOT_MNT}/BOOT.BIN"
 sudo cp ${VAR}/u-boot-sd.img "${BOOT_MNT}/u-boot.img"
 sudo cp ${VAR}/uEnv.txt "${BOOT_MNT}/"
 sudo cp ${VAR}/vmlinuz-latest "${BOOT_MNT}/zImage"
