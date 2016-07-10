dae2d7ab9f4027933c6ed9ad62e1363ff1fcb601
rektide <rektide@voodoowarez.com>
2016-07-10 23:42:53 +0000

    Install openzwave into /usr/local/ via some symlinks.

--- files/openzwave/build-openzwave.sh
+++ files/openzwave/build-openzwave.sh
@@ -2,7 +2,9 @@
 
 cd {{DIR}}
 export PREFIX="{{OPT}}"
-export LIBDIR="{{OPT}}/lib"
+#export LIBDIR="{{OPT}}/lib"
 
 make
 make install
+ln -sf {{OPT}}/lib64/* /usr/local/lib
+ln -sf {{OPT}}/include/openzwave /usr/local/include
