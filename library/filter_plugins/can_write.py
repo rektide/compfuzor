f72aeb7415c7b752bcb0e45939f74622c73ba6b5
rektide <rektide@voodoowarez.com>
2017-09-15 20:14:09 -0400

    Can write filter, start updating fs for owner, auto-become.

---
+++ library/filter_plugins/can_write.py
@@ -0,0 +1,14 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+from ansible import errors
+
+def can_write(*a, **kw):
+    return os.access(a, os.W_OK | os.X_OK)
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'can_write': can_write
+        }
