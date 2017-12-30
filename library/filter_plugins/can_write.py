d07882724b2e39ae2bf85839bb7a6fa35ee3a12b
rektide <rektide@voodoowarez.com>
2017-12-29 19:11:59 -0500

    can_write filter check file first then parent. GROUP become.

--- library/filter_plugins/can_write.py
+++ library/filter_plugins/can_write.py
@@ -4,8 +4,13 @@ __metaclass__ = type
 import os
 from ansible import errors
 
-def can_write(*a, **kw):
-    return os.access(a[0], os.W_OK | os.X_OK)
+def can_write( *a, **kw):
+    path = a[ 0]
+    mod = os.W_OK
+    if not os.access( a[0], os.F_OK):
+        path = os.path.dirname( a[0])
+        mod |= os.X_OK
+    return os.access( path, mod)
 
 class FilterModule(object):
     def filters(self):
