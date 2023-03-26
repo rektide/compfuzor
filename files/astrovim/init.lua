9508e77b8f80a779740712b3f7fa20b3bdf9ee09
jaunty wunder kind <jaunty+git@eldergods.com>
2023-03-25 23:33:24 -0400

    astrovim: tab format init.lua

--- files/astrovim/init.lua
+++ files/astrovim/init.lua
@@ -1,32 +1,41 @@
 return {
-  plugins = {
-    "jauntywunderkind/nvim-random-color",
-    "rainglow/vim",
-    "rafi/awesome-vim-colorschemes",
-    "flazz/vim-colorschemes",
-    "jose-elias-alvarez/typescript.nvim",
-    "sigmasd/deno-nvim",
-    {
-      "williamboman/mason-lspconfig.nvim",
-      opts = {
-        esnure_installed = { "tsserver", "denols" },
-      },
-    },
-  },
-  lsp = {
-    setup_handlers = {
-      denols = function(opts)
-        opts.root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
-        return opts
-      end,
-      tsserver = function(opts)
-        opts.root_dir = require("lspconfig.util").root_pattern("package.json")
-        return opts
-      end,
-      eslint = function(opts)
-        opts.root_dir = require("lspconfig.util").root_pattern(".eslintrc.json", ".eslintrc.js")
-        return opts
-      end,
-    },
-  },
+	plugins = {
+		"jauntywunderkind/nvim-random-color",
+		"rainglow/vim",
+		"rafi/awesome-vim-colorschemes",
+		"flazz/vim-colorschemes",
+		"jose-elias-alvarez/typescript.nvim",
+		"sigmasd/deno-nvim",
+		{
+			"williamboman/mason-lspconfig.nvim",
+			opts = {
+				esnure_installed = { "tsserver", "denols" },
+			},
+		},
+	},
+	lsp = {
+		setup_handlers = {
+			denols = function(opts)
+				opts.root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
+				return opts
+			end,
+			tsserver = function(opts)
+				opts.root_dir = require("lspconfig.util").root_pattern("package.json")
+				return opts
+			end,
+			eslint = function(opts)
+				opts.root_dir = require("lspconfig.util").root_pattern(".eslintrc.json", ".eslintrc.js")
+				return opts
+			end,
+		},
+	},
+	polish = function()
+		vim.filetype.add {
+			extension = {
+				pb = "yaml",
+				includes = "yaml",
+				tasks = "yaml",
+			}
+		}
+	end,
 }
