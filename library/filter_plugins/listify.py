f791045a43c791fdda0909a593c8eeb50365539f
jaunty wunder kind <jaunty+git@eldergods.com>
2024-09-23 18:15:13 -0400

    compfuzor: maybe more resillient listify filter

--- library/filter_plugins/listify.py
+++ library/filter_plugins/listify.py
@@ -2,10 +2,13 @@ import collections
 import numbers
 
 def listify(*a, **kw):
-
+    if not a[0]:
+        return []
+    if isinstance(a[0], list):
+        return a[0]
     if isinstance(a[0], dict):
         return [ {"key": k, "value": a[0][k] } for k in a[0] ]
-    return a[0]
+    return [a[0]]
    
 class FilterModule(object):
     ''' Compfuzor jinja2 filters '''
