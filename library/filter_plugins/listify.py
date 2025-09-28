d0959103ae8c0d65fbb7d5586e1de7242c0112a7
rektide de la faye <rektide@voodoowarez.com>
2025-09-28 00:07:30 -0400

    compfuzor: revamp concat

--- library/filter_plugins/listify.py
+++ library/filter_plugins/listify.py
@@ -1,19 +1,28 @@
 import collections
 import numbers
 
+
 def listify(*a, **kw):
     if not a[0]:
         return []
     if isinstance(a[0], list):
         return a[0]
     if isinstance(a[0], dict):
-        return [ {"key": k, "value": a[0][k] } for k in a[0] ]
+        return [{"key": k, "value": a[0][k]} for k in a[0]]
+    if isinstance(a[0], tuple):
+        return list(a[0])
     return [a[0]]
-   
+
+
+def concat(*a, **kw):
+    res = []
+    for item in a:
+        res = res + listify(item)
+    return res
+
+
 class FilterModule(object):
-    ''' Compfuzor jinja2 filters '''
+    """Compfuzor jinja2 filters"""
 
     def filters(self):
-        return {
-            'listify': listify
-        }
+        return {"listify": listify, "concat": concat}
