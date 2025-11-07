b6379600a6810a7ceeeae2d13f4f77c70e8d5936
rektide de la faye <rektide@voodoowarez.com>
2025-11-07 16:41:02 -0500

    compfuzor: cleanup has_vars filter

--- library/filter_plugins/vars.py
+++ library/filter_plugins/vars.py
@@ -1,120 +1,119 @@
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
 from ansible.errors import AnsibleError
-from ansible.plugins.test import TestBase
 from ansible.utils.display import Display
 from jinja2 import pass_context
 
 display = Display()
 
-class TestModule(TestBase):
 
-    def tests(self):
+class FilterModule(object):
+    def filters(self):
         return {
-            'vars': self.vars_test,
-            'vars_exists': self.vars_exists_test,
+            "has_var": self.vars_filter,
+            "has_vars": self.vars_filter,
         }
 
     @pass_context
-    def vars_test(self, context, item, prefix='', suffix=''):
-        '''
+    def vars_filter(
+        self,
+        context,
+        item,
+        prefix="",
+        suffix="",
+        returnLookedup=None,
+        upper=False,
+        lower=False,
+    ):
+        """
         Look for variables in vars and hostvars[inventory_hostname]
         If item is a string, return the variable value if found
         If item is a list, filter to only values that are found
         prefix and suffix can be added to the item name
-        '''
+        """
         # Get variables from the context
-        all_vars = context.get('vars', {})
-        
+        all_vars = context.get("vars", {})
+
         # Get hostvars for the current host
-        inventory_hostname = context.get('inventory_hostname')
-        hostvars = context.get('hostvars', {}).get(inventory_hostname, {})
-        
+        inventory_hostname = context.get("inventory_hostname")
+        hostvars = context.get("hostvars", {}).get(inventory_hostname, {})
+
         # Add debugging
-        display.vvv("vars_test: all_vars keys: %s" % list(all_vars.keys()))
-        display.vvv("vars_test: hostvars keys for %s: %s" % (inventory_hostname, list(hostvars.keys())))
-        display.vvv("vars_test: item: %s, prefix: '%s', suffix: '%s'" % (item, prefix, suffix))
-        
+        display.vvv("vars_filter: all_vars keys: %s" % list(all_vars.keys()))
+        display.vvv(
+            "vars_filter: hostvars keys for %s: %s"
+            % (inventory_hostname, list(hostvars.keys()))
+        )
+        display.vvv(
+            "vars_filter: item: %s, prefix: '%s', suffix: '%s'" % (item, prefix, suffix)
+        )
+
         def lookup_var(name):
             # Apply prefix and suffix
             var_name = prefix + name + suffix
-            display.vvv("vars_test: looking for variable: '%s'" % var_name)
-            
-            # First check in vars
-            if var_name in all_vars:
-                display.vvv("vars_test: found '%s' in all_vars: %s" % (var_name, all_vars[var_name]))
-                return all_vars[var_name]
-            # Then check in hostvars
+            if upper:
+                var_name = var_name.upper()
+            if lower:
+                var_name = var_name.lower()
+            display.vvvvv("vars_filter: looking for variable: '%s'" % var_name)
+
+            # First check in hostvars
             if var_name in hostvars:
-                display.vvv("vars_test: found '%s' in hostvars: %s" % (var_name, hostvars[var_name]))
+                display.vvvvv(
+                    "vars_filter: found '%s' in hostvars: %s"
+                    % (var_name, hostvars[var_name])
+                )
                 return hostvars[var_name]
-            display.vvv("vars_test: variable '%s' not found" % var_name)
+            # Then check in vars
+            if var_name in all_vars:
+                display.vvvvv(
+                    "vars_filter: found '%s' in all_vars: %s"
+                    % (var_name, all_vars[var_name])
+                )
+                return all_vars[var_name]
+
+            display.vvv("vars_filter: variable '%s' not found" % var_name)
             return None
-        
+
         # Handle string input
         if isinstance(item, str):
+            if returnLookedup is None:
+                returnLookedup = True
+
             result = lookup_var(item)
-            if result is not None:
+            if result is None:
+                return False
+            elif returnLookedup:
                 return result
-            return False
-        
+            else:
+                return item
+
         # Handle list input
         elif isinstance(item, list):
+            if returnLookedup is None:
+                returnLookedup = False  # just t/f
+
             # Return a list of booleans indicating if each variable exists
             results = []
             for name in item:
                 result = lookup_var(name)
-                results.append(result is not None)
-            display.vvv("vars_test: list results: %s" % results)
-            return results
-        
-        else:
-            raise AnsibleError('vars test expects a string or list, got %s' % type(item))
+                display.vvvvv(
+                    "vars_filter: list step: %s %s" % (result, result is None)
+                )
+                if result is None:
+                    display.vvvvv("SKIP")
+                elif returnLookedup:
+                    display.vvvvv("X")
+                    results.append(result)
+                else:
+                    display.vvvvv("Y")
+                    results.append(name)
 
-    @pass_context
-    def vars_exists_test(self, context, item, prefix='', suffix=''):
-        '''
-        Check if variables exist in vars and hostvars[inventory_hostname]
-        Always returns a boolean, even for lists
-        For lists, returns True only if ALL items are found
-        prefix and suffix can be added to the item name
-        '''
-        # Get variables from the context
-        all_vars = context.get('vars', {})
-        
-        # Get hostvars for the current host
-        inventory_hostname = context.get('inventory_hostname')
-        hostvars = context.get('hostvars', {}).get(inventory_hostname, {})
-        
-        # Add debugging
-        display.vvv("vars_exists_test: all_vars keys: %s" % list(all_vars.keys()))
-        display.vvv("vars_exists_test: hostvars keys for %s: %s" % (inventory_hostname, list(hostvars.keys())))
-        display.vvv("vars_exists_test: item: %s, prefix: '%s', suffix: '%s'" % (item, prefix, suffix))
-        
-        def var_exists(name):
-            # Apply prefix and suffix
-            var_name = prefix + name + suffix
-            display.vvv("vars_exists_test: checking existence of variable: '%s'" % var_name)
-            exists = (var_name in all_vars) or (var_name in hostvars)
-            display.vvv("vars_exists_test: variable '%s' exists: %s" % (var_name, exists))
-            return exists
-        
-        # Handle string input
-        if isinstance(item, str):
-            result = var_exists(item)
-            display.vvv("vars_exists_test: string result: %s" % result)
-            return result
-        
-        # Handle list input
-        elif isinstance(item, list):
-            # Return True only if ALL items exist
-            for name in item:
-                if not var_exists(name):
-                    display.vvv("vars_exists_test: list result: False (missing: %s)" % name)
-                    return False
-            display.vvv("vars_exists_test: list result: True")
-            return True
-        
+            display.vvvvv("vars_filter: list results: %s" % results)
+            return results
         else:
-            raise AnsibleError('vars_exists test expects a string or list, got %s' % type(item))
+            raise AnsibleError(
+                "vars test expects a string or list, got %s" % type(item)
+            )
